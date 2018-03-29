5e43e7b9d3926d293c950ff77b3495f2561e1a96
Preimesberger, Freya M <fp4364@lib-pclem031.lib.utexas.edu>
2018-03-28 19:13:15 -0500

    Initial Commit

---
+++ Group1Alpha/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Group1Alpha
+//
+//  Created by Preimesberger, Freya M on 3/28/18.
+//  Copyright © 2018 Preimesberger, Freya M. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
